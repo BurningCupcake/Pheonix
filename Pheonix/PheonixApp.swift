6c0062051598c48c9f88fa1ef59dbf8d6037425b
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-01 17:50:59 -0700

    Initial Commit

---
+++ Pheonix/PheonixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PheonixApp.swift
+//  Pheonix
+//
+//  Created by Justin Fortier on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PheonixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
