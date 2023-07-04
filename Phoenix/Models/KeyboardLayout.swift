35f780944e5f9730156484d620ab77a9ec552663
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-04 11:53:46 -0700

    1

--- Phoenix/Models/KeyboardLayout.swift
+++ Phoenix/Models/KeyboardLayout.swift
@@ -8,6 +8,15 @@ struct KeyboardLayout {
     static func defaultLayout() -> KeyboardLayout {
         // Define the default keyboard layout
         
-        return KeyboardLayout(layout: [], keySize: CGSize.zero, keyPadding: 0)
+        let defaultLayout: [[String]] = [
+            ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+            ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+            ["Z", "X", "C", "V", "B", "N", "M"]
+        ]
+        
+        let keySize = CGSize(width: 40, height: 40)
+        let keyPadding: CGFloat = 5
+        
+        return KeyboardLayout(layout: defaultLayout, keySize: keySize, keyPadding: keyPadding)
     }
 }
--- Phoenix/Views/KeyboardView.swift
+++ Phoenix/Views/KeyboardView.swift
@@ -2,16 +2,29 @@ import SwiftUI
 
 struct KeyboardView: View {
     var keyboardInteraction: KeyboardInteraction
-    weak var delegate: KeyboardViewDelegate? // Add the delegate property
+    weak var delegate: KeyboardViewDelegate?
+    
+    var keyboardLayout: KeyboardLayout = KeyboardLayout.defaultLayout()
     
     var body: some View {
-        // Keyboard view UI code goes here
         VStack {
             Text("Keyboard View")
                 .font(.title)
                 .padding()
             
             // Add keys based on the keyboard layout and interaction
+            ForEach(keyboardLayout.layout.indices, id: \.self) { rowIndex in
+                HStack {
+                    ForEach(keyboardLayout.layout[rowIndex].indices, id: \.self) { columnIndex in
+                        Text(keyboardLayout.layout[rowIndex][columnIndex])
+                            .tag(rowIndex * keyboardLayout.layout[rowIndex].count + columnIndex)
+                            .onTapGesture {
+                                let key = keyboardLayout.layout[rowIndex][columnIndex]
+                                delegate?.didSelectKey(key)
+                            }
+                    }
+                }
+            }
         }
     }
 }
