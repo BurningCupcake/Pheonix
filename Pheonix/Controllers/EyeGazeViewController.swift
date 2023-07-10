1d65c4d25427251df97aff758d9da3ef438e43e4
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-10 14:51:14 -0700

    Update EyeGazeViewController.swift

--- Pheonix/Controllers/EyeGazeViewController.swift
+++ Pheonix/Controllers/EyeGazeViewController.swift
@@ -9,45 +9,45 @@ class EyeGazeViewController: UIViewController, GazeDetectionDelegate, KeyboardIn
     private var wordSuggestion: WordSuggestion!
     private var eyeTrackingController: EyeTrackingController!
     private var keyboardView: KeyboardView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         dynamicCalibration = DynamicCalibration()
         gazeDetection = GazeDetection(calibrationDelegate: dynamicCalibration)
         keyboardInteraction = KeyboardInteraction(layout: KeyboardView.defaultLayout)
         textEntry = TextEntry()
         wordSuggestion = WordSuggestion()
         eyeTrackingController = EyeTrackingController()
-
+        
         gazeDetection.delegate = self
         dynamicCalibration.delegate = eyeTrackingController
         keyboardInteraction.delegate = textEntry
         wordSuggestion.delegate = eyeTrackingController
-
+        
         keyboardView = KeyboardView()
         keyboardView.delegate = keyboardInteraction
         keyboardView.backgroundColor = .lightGray
-
+        
         view.addSubview(keyboardView)
-
+        
         gazeDetection.start()
     }
-
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         keyboardView.frame = view.bounds
     }
-
+    
     func gazeDetection(_ gazeDetection: GazeDetection, didDetectGazeAt point: CGPoint) {
         keyboardInteraction.processGazePoint(point)
     }
-
+    
     func keyboardInteraction(_ keyboardInteraction: KeyboardInteraction, didSelectKey key: String) {
         textEntry.keyboardInteraction(keyboardInteraction, didSelectKey: key)
         wordSuggestion.processTextEntry(textEntry)
     }
-
+    
     func wordSuggestion(_ wordSuggestion: WordSuggestion, didSuggestWords words: [String]) {
         keyboardView.updateWordSuggestions(words)
     }
