6a15ba66f83614e9237763ca946cb464a8cfcf74
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-10 16:26:30 -0700

    si

--- Pheonix/Services/KeyboardInteraction.swift
+++ Pheonix/Services/KeyboardInteraction.swift
@@ -11,7 +11,7 @@ class KeyboardInteraction {
     func processGazePoint(_ point: CGPoint) {
         let keyIndex = calculateKeyIndex(at: point)
         if let key = getKey(at: keyIndex) {
-            delegate?.didSelectKey(key)
+            delegate?.keyboardInteraction(self, didSelectKey: key)
         }
     }
     
@@ -21,17 +21,17 @@ class KeyboardInteraction {
         
         let row = Int(point.y / (keySize.height + keyPadding))
         let column = Int(point.x / (keySize.width + keyPadding))
-                         
-            return row * layout.layout[row].count + column
-                         }
-                         
+        
+        return row * layout.layout[row].count + column
+    }
+    
     private func getKey(at index: Int) -> String? {
         let flattenedLayout = layout.layout.flatMap { $0 }
-            
-            guard index >= 0, index < flattenedLayout.count else {
-                return nil
-            }
-            
-            return flattenedLayout[index]
+        
+        guard index >= 0, index < flattenedLayout.count else {
+            return nil
         }
-                         }
+        
+        return flattenedLayout[index]
+    }
+}
