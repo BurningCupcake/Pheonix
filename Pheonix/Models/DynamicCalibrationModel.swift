8194a66e186276276561689e0714b9b0f8da9231
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-12 17:27:36 -0700

    Update DynamicCalibrationModel.swift
    Signed-off-by: BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>

--- Pheonix/Models/DynamicCalibrationModel.swift
+++ Pheonix/Models/DynamicCalibrationModel.swift
@@ -1,6 +1,47 @@
-import Combine
-import SwiftUI
+import UIKit
 
-class DynamicCalibrationModel: ObservableObject {
-    @Published var calibrationPoints: [CGPoint] = []
+class DynamicCalibrationModel {
+    private var calibrationPoints: [CGPoint] = []
+    
+    func addCalibrationPoint(_ point: CGPoint) {
+        calibrationPoints.append(point)
+    }
+    
+    func clearCalibrationPoints() {
+        calibrationPoints.removeAll()
+    }
+    
+    func calculateCalibrationResult() -> CalibrationResult? {
+        guard !calibrationPoints.isEmpty else {
+            return nil
+        }
+        
+        // Perform calibration calculation based on calibration points
+        // Return the calibration result
+        
+        // Example implementation:
+        let result = CalibrationResult(calibrationPoints: calibrationPoints)
+        return result
+    }
+    
+    func performCalibration(completion: @escaping (Bool) -> Void) {
+        let fractalLayer = CALayer()
+        fractalLayer.frame = UIScreen.main.bounds
+        fractalLayer.backgroundColor = UIColor.white.cgColor
+        fractalLayer.contentsGravity = .center
+        fractalLayer.contentsScale = UIScreen.main.scale
+        fractalLayer.masksToBounds = true
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let keyboardView = windowScene.windows.first?.rootViewController?.view {
+            keyboardView.layer.insertSublayer(fractalLayer, at: 0)
+        }
+        
+        UIView.animate(withDuration: 3.0, animations: {
+            fractalLayer.backgroundColor = UIColor.red.cgColor
+        }) { (_) in
+            fractalLayer.removeFromSuperlayer()
+            completion(true)
+        }
+    }
 }
