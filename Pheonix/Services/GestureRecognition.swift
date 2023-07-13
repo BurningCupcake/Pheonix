bfff43ca0ba96cb3620e0edcd96b311db5aa750a
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-12 17:37:49 -0700

    Update GestureRecognition.swift
    Signed-off-by: BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>

--- Pheonix/Services/GestureRecognition.swift
+++ Pheonix/Services/GestureRecognition.swift
@@ -1,48 +1,75 @@
+import Foundation
 import UIKit
-import Vision
+
+protocol GestureRecognitionDelegate: AnyObject {
+    func didRecognizeGesture(_ gesture: GestureType)
+}
 
 class GestureRecognition {
     weak var delegate: GestureRecognitionDelegate?
-    private let gestureClassifier: GestureClassifier
     
-    init(gestureClassifier: GestureClassifier) {
-        self.gestureClassifier = gestureClassifier
+    // Constants for gesture recognition
+    private let blinkDurationThreshold: TimeInterval = 0.3
+    private let swipeThreshold: CGFloat = 50.0
+    
+    private var initialTouchPoint: CGPoint?
+    private var initialTouchTime: TimeInterval?
+    
+    func processTouchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        initialTouchPoint = touch.location(in: touch.view)
+        initialTouchTime = touch.timestamp
     }
     
-    func processGesture(image: UIImage) {
-        guard let cgImage = image.cgImage else {
+    func processTouchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first,
+              let initialPoint = initialTouchPoint,
+              let initialTime = initialTouchTime
+        else {
             return
         }
         
-        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        let finalPoint = touch.location(in: touch.view)
+        let finalTime = touch.timestamp
         
-        let request = VNRecognizeTextRequest(completionHandler: handleGestureRecognition)
+        let duration = finalTime - initialTime
+        let distance = calculateDistance(from: initialPoint, to: finalPoint)
         
-        do {
-            try requestHandler.perform([request])
-        } catch {
-            print("Error processing gesture: \(error)")
+        if duration <= blinkDurationThreshold {
+            delegate?.didRecognizeGesture(.blink)
+        } else if distance >= swipeThreshold {
+            detectSwipeGesture(from: initialPoint, to: finalPoint)
         }
+        
+        // Reset values
+        initialTouchPoint = nil
+        initialTouchTime = nil
     }
     
-    private func handleGestureRecognition(request: VNRequest, error: Error?) {
-        guard let results = request.results as? [VNRecognizedTextObservation] else {
-            return
-        }
+    private func calculateDistance(from startPoint: CGPoint, to endPoint: CGPoint) -> CGFloat {
+        let deltaX = endPoint.x - startPoint.x
+        let deltaY = endPoint.y - startPoint.y
         
-        var recognizedGesture: Gesture?
+        return sqrt(pow(deltaX, 2) + pow(deltaY, 2))
+    }
+    
+    private func detectSwipeGesture(from startPoint: CGPoint, to endPoint: CGPoint) {
+        let horizontalDistance = endPoint.x - startPoint.x
+        let verticalDistance = endPoint.y - startPoint.y
         
-        for result in results {
-            guard let topCandidate = result.topCandidates(1).first else {
-                continue
+        if abs(horizontalDistance) > abs(verticalDistance) {
+            if horizontalDistance > 0 {
+                delegate?.didRecognizeGesture(.swipeRight)
+            } else {
+                delegate?.didRecognizeGesture(.swipeLeft)
             }
-            
-            if gestureClassifier.isGesture(topCandidate.string) {
-                recognizedGesture = Gesture(name: topCandidate.string)
-                break
+        } else {
+            if verticalDistance > 0 {
+                delegate?.didRecognizeGesture(.swipeDown)
+            } else {
+                delegate?.didRecognizeGesture(.swipeUp)
             }
         }
-        
-        delegate?.gestureRecognition(self, didRecognizeGesture: recognizedGesture)
     }
 }
