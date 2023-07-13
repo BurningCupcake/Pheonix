0d4f171d6ee104104678d8a2343bea7940c9ee9a
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-12 17:51:11 -0700

    Update PredictiveTextServerice.swift
    Signed-off-by: BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>

--- Pheonix/Services/PredictiveTextServerice.swift
+++ Pheonix/Services/PredictiveTextServerice.swift
@@ -9,9 +9,19 @@ class PredictiveTextService {
     }
     
     func updatePredictiveText(for text: String) {
-        // Update predictive text state based on the provided text
-        let predictiveTextState = PredictiveTextState(suggestions: []) // Replace this with your logic
+        let suggestedWords = generatePredictiveText(for: text)
+        let predictiveTextState = PredictiveTextState(suggestions: suggestedWords)
         
         predictiveTextStateSubject.send(predictiveTextState)
+    }
+    
+    private func generatePredictiveText(for text: String) -> [String] {
+        // TODO: Implement your logic to generate predictive text suggestions based on the provided text
+        // You can use algorithms, language models, or any other technique that suits your requirements
+        // Return an array of suggested words
+        
+        // Placeholder implementation:
+        let suggestedWords = ["example1", "example2", "example3"]
+        return suggestedWords
     }
 }
