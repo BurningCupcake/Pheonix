4ff4f4e4c069013d39cd7bd3979f4a3352a47eba
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-12 17:26:36 -0700

    Update AppState.swift
    Signed-off-by: BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>

--- Pheonix/Models/AppState.swift
+++ Pheonix/Models/AppState.swift
@@ -1,23 +1,46 @@
 import Foundation
 
-struct TextEntryState {
-    var text: String
-}
-
-struct PredictiveTextState: Equatable {
-    var suggestions: [String]
+class AppState {
+    static let shared = AppState()
     
-    static let empty = PredictiveTextState(suggestions: [])
-}
-
-struct AppState {
-    var textEntryState: TextEntryState
-    var predictiveTextState: PredictiveTextState
+    // Keyboard state properties
+    var isCapsLockEnabled: Bool = false
+    var isShiftEnabled: Bool = false
+    var selectedLanguage: Language = .english
+    var predictiveTextSuggestions: [String] = []
     
-    // Additional properties and initializers
+    // User preferences
+    var isDarkModeEnabled: Bool = false
     
-    init(textEntryState: TextEntryState, predictiveTextState: PredictiveTextState) {
-        self.textEntryState = textEntryState
-        self.predictiveTextState = predictiveTextState
+    private init() {
+        // Initialize any necessary state properties here
+    }
+    
+    // Update keyboard state based on user actions
+    func toggleCapsLock() {
+        isCapsLockEnabled = !isCapsLockEnabled
+        // Perform any necessary updates or actions when caps lock is toggled
+    }
+    
+    func toggleShift() {
+        isShiftEnabled = !isShiftEnabled
+        // Perform any necessary updates or actions when shift is toggled
+    }
+    
+    func changeLanguage(to language: Language) {
+        selectedLanguage = language
+        // Perform any necessary updates or actions when language is changed
+    }
+    
+    // Update predictive text suggestions
+    func updatePredictiveTextSuggestions(_ suggestions: [String]) {
+        predictiveTextSuggestions = suggestions
+        // Perform any necessary updates or actions when predictive text suggestions are updated
+    }
+    
+    // User preference methods
+    func toggleDarkMode() {
+        isDarkModeEnabled = !isDarkModeEnabled
+        // Perform any necessary updates or actions when dark mode is toggled
     }
 }
