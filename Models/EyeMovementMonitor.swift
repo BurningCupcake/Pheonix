6c24269863135b95a244b5249e409db180584dc1
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-28 09:48:25 -0700

    dfgv

--- Models/EyeMovementMonitor.swift
+++ Models/EyeMovementMonitor.swift
@@ -1,8 +1,21 @@
-//
-//  EyeMovementMonitor.swift
-//  Pheonix
-//
-//  Created by Justin Fortier on 7/28/23.
-//
-
-import Foundation
+class EyeMovementMonitor {
+    var eyeTracker: EyeTracker
+    var timer: Timer?
+    var eyeMovements: [EyeMovement] = []
+    
+    init(eyeTracker: EyeTracker) {
+        self.eyeTracker = eyeTracker
+    }
+    
+    func startMonitoring() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            let currentPosition = self.eyeTracker.currentPosition
+            self.eyeMovements.append(currentPosition) // Assuming EyeMovement is CGPoint
+        }
+    }
+    
+    func stopMonitoring() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
--- Services/DynamicCalibration.swift
+++ Services/DynamicCalibration.swift
@@ -3,21 +3,31 @@ import UIKit
 class DynamicCalibration: CalibrationDelegate {
     private var calibrationPoints: [CGPoint] = []
     
-    init() {}
+    init(
+    ) {}
     
-    static func create() -> DynamicCalibration {
-        return DynamicCalibration()
+    static func create(
+    ) -> DynamicCalibration {
+        return DynamicCalibration(
+        )
     }
     
-    func addCalibrationPoint(_ point: CGPoint) {
-        calibrationPoints.append(point)
+    func addCalibrationPoint(
+        _ point: CGPoint
+    ) {
+        calibrationPoints.append(
+            point
+        )
     }
     
-    func clearCalibrationPoints() {
-        calibrationPoints.removeAll()
+    func clearCalibrationPoints(
+    ) {
+        calibrationPoints.removeAll(
+        )
     }
     
-    func calculateCalibrationResult() -> CalibrationResult? {
+    func calculateCalibrationResult(
+    ) -> CalibrationResult? {
         guard !calibrationPoints.isEmpty else {
             return nil
         }
@@ -26,42 +36,131 @@ class DynamicCalibration: CalibrationDelegate {
         // Return the calibration result
         
         // Example implementation:
-        let result = CalibrationResult(calibrationPoints: calibrationPoints)
+        let result = CalibrationResult(
+            calibrationPoints: calibrationPoints
+        )
         return result
+    }
+    
+    
+    func generateFractal(size: CGSize) -> UIImage {
+        let width = Int(size.width)
+        let height = Int(size.height)
+        let scale = 4.0
+        let maxIter = 256
+        
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        for x in 0..<width {
+            for y in 0..<height {
+                var xx = 0.0
+                var yy = 0.0
+                let cx = Double(x) / Double(width) * scale - scale / 2
+                let cy = Double(y) / Double(height) * scale - scale / 2
+                
+                var iter = 0
+                while xx*xx + yy*yy <= 4 && iter < maxIter {
+                    let temp = xx*xx - yy*yy + cx
+                    yy = 2*xx*yy + cy
+                    xx = temp
+                    iter += 1
+                }
+                
+                var color = UIColor.white
+                if iter < maxIter {
+                    color = UIColor(hue: CGFloat(iter) / CGFloat(maxIter), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+                }
+                
+                context.setFillColor(color.cgColor)
+                context.fill(CGRect(x: x, y: y, width: 1, height: 1))
+            }
+        }
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
     }
     
     func performCalibration(in viewController: UIViewController, completion: @escaping (Bool) -> Void) {
         let fractalLayer = CALayer()
-        fractalLayer.frame = viewController.view.bounds // Use the bounds of the provided view controller
+        fractalLayer.frame = viewController.view.bounds
         fractalLayer.backgroundColor = UIColor.white.cgColor
-        fractalLayer.contentsGravity = .center
-        fractalLayer.contentsScale = viewController.view.contentScaleFactor // Use the scale of the provided view controller
-        fractalLayer.masksToBounds = true
+        
+        let fractalImage = generateFractal(size: viewController.view.bounds.size, iterations: 100)
+        fractalLayer.contents = fractalImage.cgImage
         
         viewController.view.layer.insertSublayer(fractalLayer, at: 0)
         
-        UIView.animate(withDuration: 3.0, animations: {
-            fractalLayer.backgroundColor = UIColor.red.cgColor
-        }) { (_) in
+        animateFractalGrowth(fractalLayer)
+        incorporateKeys(fractalLayer)
+        
+        animateColorShift(layer: fractalLayer,
+                          fromColor: .white,
+                          toColor: .red,
+                          duration: 3.0)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
             fractalLayer.removeFromSuperlayer()
             completion(true)
         }
     }
     
+    func animateFractalGrowth(_ layer: CALayer) {
+        let growthAnimation = CABasicAnimation(keyPath: "transform.scale.y")
+        growthAnimation.fromValue = 0.0
+        growthAnimation.toValue = 1.0
+        growthAnimation.duration = 3.0
+        layer.add(growthAnimation, forKey: "transform.scale.y")
+    }
+    
+    func incorporateKeys(_ layer: CALayer) {
+        // For instance, if keys are images, you can add them as sublayers here.
+    }
+    
+    func animateColorShift(layer: CALayer, fromColor: UIColor, toColor: UIColor, duration: TimeInterval) {
+        let colorChangeAnimation = CABasicAnimation(keyPath: "backgroundColor")
+        colorChangeAnimation.fromValue = fromColor.cgColor
+        colorChangeAnimation.toValue = toColor.cgColor
+        colorChangeAnimation.duration = duration
+        layer.add(colorChangeAnimation, forKey: "backgroundColor")
+    }
+    
+    func finalizeCalibration() {
+        // Here you can finalize the calibration process, for example by saving the CalibrationData.
+    }
+    
+    func determinePointOfInterest(eyeMovements: [EyeMovement], fractalState: FractalState) -> CGPoint {
+        // Here you would analyze the eye movements and fractal state to determine the point of interest.
+        return .zero
+    }
+
+    
     // MARK: - CalibrationDelegate methods
     
-    func didStartCalibration() {
+    func didStartCalibration(
+    ) {
         // Handle the start of the calibration process
-        print("Calibration started")
+        print(
+            "Calibration started"
+        )
     }
     
-    func didCompleteCalibration(withResult result: CalibrationResult) {
+    func didCompleteCalibration(
+        withResult result: CalibrationResult
+    ) {
         // Handle the completion of the calibration process with the provided result
-        print("Calibration completed with result: \(result)")
+        print(
+            "Calibration completed with result: \(result)"
+        )
     }
     
-    func didFailCalibration(withError error: Error) {
+    func didFailCalibration(
+        withError error: Error
+    ) {
         // Handle the failure of the calibration process with the provided error
-        print("Calibration failed with error: \(error)")
+        print(
+            "Calibration failed with error: \(error)"
+        )
     }
 }
