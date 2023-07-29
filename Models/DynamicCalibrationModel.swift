c3f33b066200646727050a3d58a45333a7676124
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-07-29 10:58:26 -0700

    modified:   Models/DynamicCalibrationModel.swift

--- Models/DynamicCalibrationModel.swift
+++ Models/DynamicCalibrationModel.swift
@@ -1,70 +1,63 @@
+private var calibrationSubscription: AnyCancellable?
 
-import Combine
-import SwiftUI
-
-class DynamicCalibrationModel: ObservableObject {
-    @Published var calibrationPoints: [CGPoint] = []
-    
-    private var isCalibrationInProgress = false
-    
-    func startCalibration() {
-        guard !isCalibrationInProgress else {
-            return
-        }
-        
-        isCalibrationInProgress = true
-        
-        // Perform the calibration process
-        showCalibrationVisualization()
-        simulateCalibrationDataCollection()
-        
-        // After data collection, calculate the calibration result
-        let calibrationResult = calculateCalibrationResult()
-        
-        // Update the calibration state and apply the result if successful
-        if let result = calibrationResult {
-            applyCalibrationResult(result)
-        }
-        
-        isCalibrationInProgress = false
+func startCalibration() {
+    guard !isCalibrationInProgress else {
+        return
     }
     
-    private func showCalibrationVisualization() {
-        // Show the captivating fractal visualization on the screen
-        // Customize this method based on your implementation
-    }
+    isCalibrationInProgress = true
     
-    private func simulateCalibrationDataCollection() {
-        // Simulate data collection by adding sample calibration points
-        // Customize this method based on your implementation
-        
-        // Example implementation:
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            let simulatedPoints = [
-                CGPoint(x: 100, y: 100),
-                CGPoint(x: 200, y: 300),
-                CGPoint(x: 300, y: 200)
-            ]
+    showCalibrationVisualization()
+    simulateCalibrationDataCollection()
+    
+    calibrationSubscription = $calibrationPoints
+        .sink { [weak self] points in
+            guard let self = self else { return }
             
-            self.calibrationPoints = simulatedPoints
+            let calibrationResult = self.calculateCalibrationResult(points: points)
+            if let result = calibrationResult {
+                self.applyCalibrationResult(result: result)
+            }
+            
+            self.isCalibrationInProgress = false
         }
+}
+
+private func showCalibrationVisualization() {
+    // Show the captivating fractal visualization on the screen
+    // Customize this method based on your implementation
+    // Example implementation:
+    print("Showing calibration visualization")
+}
+
+private func simulateCalibrationDataCollection() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        let simulatedPoints = [
+            CGPoint(x: 100, y: 100),
+            CGPoint(x: 200, y: 300),
+            CGPoint(x: 300, y: 200)
+        ]
+        
+        self.calibrationPoints = simulatedPoints
+    }
+}
+
+private func calculateCalibrationResult(points: [CGPoint]) -> CalibrationResult? {
+    guard points.count >= 3 else {
+        return nil
     }
     
-    private func calculateCalibrationResult() -> CalibrationResult? {
-        guard calibrationPoints.count >= 3 else {
-            return nil
-        }
-        
-        // Perform the calibration calculation based on calibration points
-        // Return the calibration result
-        
-        // Example implementation:
-        let result = CalibrationResult(calibrationPoints: calibrationPoints)
-        return result
-    }
-    
-    private func applyCalibrationResult(_ result: CalibrationResult) {
-        // Apply the calibration result to the eye-tracking system
-        // Customize this method based on your implementation
-    }
+    // Perform the calibration calculation based on calibration points
+    // Return the calibration result
+    // Example implementation:
+    let result = CalibrationResult(calibrationPoints: points)
+    return result
+}
+
+private func applyCalibrationResult(result: CalibrationResult) {
+    // Apply the calibration result to the eye-tracking system
+    // Customize this method based on your implementation
+    // Example implementation:
+    print("Applying calibration result")
+}
 }
