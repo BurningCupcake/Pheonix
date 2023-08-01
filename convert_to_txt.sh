7d7161c58c6df5787cda62b58e60001f09d4a339
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-08-01 15:38:54 -0700

    aDASX

--- convert_to_txt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a new folder for the converted files
-mkdir -p ConvertedSwiftFiles
-
-# Find all Swift files and copy them to the new folder with .txt extension
-find . -name "*.swift" -exec sh -c 'file="{}"; output="ConvertedSwiftFiles/$(basename "$file" .swift).txt"; cp "$file" "$output"' \;
