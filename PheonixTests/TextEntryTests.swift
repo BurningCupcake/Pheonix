117c689f8d3e1cfeb20bb103d6589072d582e697
BurningCupcake <125168971+BurningCupcake@users.noreply.github.com>
2023-11-18 12:17:06 -0800

    Fix Files

--- PheonixTests/TextEntryTests.swift
+++ PheonixTests/TextEntryTests.swift
@@ -1,12 +1,12 @@
 import XCTest
 
-
 class TextEntryTests: XCTestCase {
     var textEntry: TextEntry!
     
     override func setUp() {
         super.setUp()
         textEntry = TextEntry()
+        textEntry.appendText("Hello")  // Initialize textEntry with some initial text
     }
     
     override func tearDown() {
@@ -16,15 +16,14 @@ class TextEntryTests: XCTestCase {
     
     func testTextEntry_TextAppended_CorrectText() {
         // Given
-        let initialText = "Hello"
         let appendedText = " World"
         
         // When
         textEntry.appendText(appendedText)
         
         // Then
-        XCTAssertEqual(textEntry.currentText, initialText + appendedText, "The appended text should be correctly added to the current text.")
+        XCTAssertEqual(textEntry.currentText, "Hello World", "The appended text should be correctly added to the current text.")
     }
     
-    // Add more test cases to cover other functionalities and scenarios of the TextEntry class
+    // Additional test cases...
 }
